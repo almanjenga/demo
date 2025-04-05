bf8341efc3fe56ec91dec6c126b9aa47fb9f3d04
Charlene393 <charlenembugua0@gmail.com>
2025-04-05 22:41:12 +0300

    Testing Hero

---
+++ components/Hero.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+
+const Hero = () => {
+    return (
+        <div className="hero">
+            <div className="flex-1 pt-36 padding-x">
+                <h1 className="hero__title">
+                🌞 AI SOLAR WATER ASSISTANT
+                </h1>
+            </div>
+        </div>
+    )
+}
+
+export default Hero

50b3ea903b5d5d39180a5e0169e5da1e0e810477
Charlene393 <charlenembugua0@gmail.com>
2025-04-11 20:13:06 +0300

    Removed the sign up page

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,23 +1,29 @@
-"use client";
+    "use client";
 
-import Image from "next/image"
-import { CustomButtonProps } from "../types";
+    import { CustomButtonProps } from "../types";
+    import React from "react";
 
-
-const CustomButton = ({title, containerStyles, handleClick, btnType}: CustomButtonProps) => {
+    const CustomButton = ({
+    title,
+    containerStyles,
+    handleClick,
+    btnType,
+    children, // added children support
+    }: CustomButtonProps & { children?: React.ReactNode }) => {
     return (
-    <button
-    disabled={false}
-    type={btnType || "button"}
-    className={`custom-btn flex flex-row relative justify-center items-center py-1 px-6 outline-none ${containerStyles}`}
-    onClick={handleClick}
->
-    <span className="flex-1">
-    {title}
-    </span>
-</button>
+        <button
+        disabled={false}
+        type={btnType || "button"}
+        className={`custom-btn flex flex-row justify-center items-center py-2 px-6 outline-none ${containerStyles}`}
+        onClick={handleClick}
+        >
+        {children ? (
+            children // render icons or other custom JSX
+        ) : (
+            <span className="flex-1">{title}</span>
+        )}
+        </button>
+    );
+    };
 
-    )
-}
-
-export default CustomButton
+    export default CustomButton;
--- types/index.ts
+++ types/index.ts
@@ -6,5 +6,6 @@ export interface CustomButtonProps {
     handleClick?: 
     MouseEventHandler<HTMLButtonElement>;
     btnType?: "button" | "submit" ;
+    children?: React.ReactNode;
 
 }

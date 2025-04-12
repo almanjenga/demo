9eedae8d0b19c374cd5750f62800575b1f49fb0a
Charlene393 <charlenembugua0@gmail.com>
2025-04-12 10:12:16 +0300

    added a react icon

--- components/chat-bot/CustomChatBot.tsx
+++ components/chat-bot/CustomChatBot.tsx
@@ -4,8 +4,8 @@ import {TbMessageChatbot} from "react-icons/tb"
 
 export  default function CustomChatBot() {
     return(
-        <>
-        <TbMessageChatbot/>
-        </>
+        <div className="flex justify-center items-center">
+            <TbMessageChatbot size={48} className="text-blue-600" />
+        </div>
     )
 }
--- src/app/chat-bot/page.tsx
+++ src/app/chat-bot/page.tsx
@@ -5,9 +5,8 @@ const ChatBot = () => {
     return (
         <div className="flex items-center justify-center min-h-screen bg-gray-200 px-4">
             <div className="bg-white w-full max-w-4xl p-10 rounded-3xl shadow-lg text-center">
-                <h1 className="text-4xl font-bold text-green-600 mb-4">Chatbot</h1>
-                <p className="text-lg text-gray-700 mb-6">Powered by OpenAI</p>
-                <CustomChatBot />
+                <h1 className="text-4xl font-bold text-blue-600 mb-4">Chatbot</h1>
+                <CustomChatBot/>
             </div>
         </div>
     );

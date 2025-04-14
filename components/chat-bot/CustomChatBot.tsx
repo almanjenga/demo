0c45588e1ece8697cdc600f3ed0191ed316726f8
Charlene393 <charlenembugua0@gmail.com>
2025-04-14 09:17:51 +0300

    Chatbot layout edit

--- components/chat-bot/CustomChatBot.tsx
+++ components/chat-bot/CustomChatBot.tsx
@@ -1,11 +1,31 @@
 "use client";
+import { useState } from "react";
 import {TbMessageChatbot} from "react-icons/tb"
 
 
+
 export  default function CustomChatBot() {
+    const [showChat, setShowChat] = useState(true);
     return(
-        <div className="flex justify-center items-center">
-            <TbMessageChatbot size={48} className="text-blue-600" />
+        <div className="w-screen h-screen">
+            <TbMessageChatbot 
+                size={64} 
+                className="text-blue-600 fixed bottom-6 right-6 cursor-pointer hover:scale-110 transition-transform duration-200" 
+            />
+            {
+                showChat && (
+                    <div>
+                        <div>
+                           {/* Chatbot header */} 
+                            <div>
+                            <h2>Chatbot </h2>
+                            <p>Davis and Shirtliff Ai</p>
+                            </div>
+                        </div>
+                    </div>
+                )
+
+            }
         </div>
     )
 }
